80f3cd4bd32ff4f59a021799eda22950ba50404b
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-02-11 08:27:45 +0530

    Conditionally set variable representing the parent of vault, mint etc.
    For historical reasons, the parent of vault, mint etc on the
    production server was called bank/. But on our local machines, it
    was changed to be called 'gutenberg'. The environment variable
    - PRODUCTION_SERVER - identifies the machine as such. It does NOT
    - in fact, should not - be set on your local machine. Only on the
    production server

--- shared/individual.mk
+++ shared/individual.mk
@@ -5,7 +5,15 @@ latexCmd := latex
 ps2pdfCmd := ps2pdf
 
 # Locations ... 
-Gutenberg := $(HOME)/gutenberg
+
+# For historical reasons, the parent of vault, mint etc on the 
+# production server was called bank/. But on our local machines, it 
+# was changed to be called 'gutenberg'. The environment variable 
+# - PRODUCTION_SERVER - identifies the machine as such. It does NOT 
+# - in fact, should not - be set on your local machine. Only on the
+# production server
+
+Gutenberg := $(if $(PRODUCTION_SERVER), $(HOME)/bank, $(HOME)/gutenberg)
 Vault := $(Gutenberg)/vault
 Shared := $(Gutenberg)/shared
 
