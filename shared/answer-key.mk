a133ee2d93394715a526f4361893bd258b636280
Abhinav Chaturvedi <abhinav.devel@gmail.com>
2012-04-16 13:44:45 +0530

    Let ghostscript generate the per-page previews for the answer-key
    Keeping my fingers crossed that this works on Linode - the only place
    where I have enough data to test these changes with

--- shared/answer-key.mk
+++ shared/answer-key.mk
@@ -1,21 +1,16 @@
 
 PREVIEWS := $(patsubst %.tex, %-preview.jpeg, $(wildcard *.tex))
-THUMBNAILS := $(patsubst %.tex, %-thumbnail.jpeg, $(wildcard *.tex))
+#THUMBNAILS := $(patsubst %.tex, %-thumbnail.jpeg, $(wildcard *.tex))
 
-.PHONY : preview thumbnail install
+.PHONY : preview install
 
-install : thumbnail install-pdfs
+install : preview install-pdfs
 	-@mkdir $(CURDIR)/../preview 
 	@mv $(CURDIR)/page-*.jpeg $(CURDIR)/../preview/
 
-thumbnail : preview $(THUMBNAILS)
-$(THUMBNAILS) : %-thumbnail.jpeg : %-preview.jpeg
-	@convert $< -resize 120x120 $@
-
-preview : ps pdf $(PREVIEWS)
-$(PREVIEWS) : %-preview.jpeg : %.ps
-	@gs -sDEVICE=jpeg -r700 -o $@ $<
-	@convert $@ -resize 600x800 $@
+preview : pdf 
+	@gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -r700 -sOutputFile=page-%d.jpeg $(PDF_FILES)
+	@for f in `ls page-*.jpeg`; do convert $$f -resize 600x800 $$f ; done 
 
 include test-paper.mk 
 
