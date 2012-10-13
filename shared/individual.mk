21e8f9b8718dbe32bb0f616ad94de08d3ba2f122
Akshay Damarla <akshay.damarla@gmail.com>
2012-10-13 23:50:32 +0530

    - made all paths to bank root common across all machines and for all users   the path to bank is always going to be pointed to by  /opt/gutenberg/bank

--- shared/individual.mk
+++ shared/individual.mk
@@ -1,11 +1,7 @@
 
 # /opt/gutenberg/PRODUCTION_SERVER is a zero size file on Linode only !!
 
-ifeq ($(realpath /opt/gutenberg/PRODUCTION_SERVER),)
-  Gutenberg := /home/abhinav/workspace/gutenberg-live
-else
-  Gutenberg := /home/gutenberg/bank
-endif 
+Gutenberg := /opt/gutenberg/bank
 
 include $(join $(strip $(Gutenberg)), /shared/environment.mk)
 
