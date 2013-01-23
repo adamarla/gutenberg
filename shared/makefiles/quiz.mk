0c4304a0c686ac37b6000fb49329e7f88ae9d749
Akshay Damarla <akshay.damarla@gmail.com>
2013-01-23 12:47:57 +0530

    - changed quiz.mk to create blurry fillers for student console

--- shared/makefiles/quiz.mk
+++ shared/makefiles/quiz.mk
@@ -9,13 +9,20 @@ DVI_FILES := $(patsubst %.tex, %.dvi, $(wildcard *.tex))
 PS_FILES := $(patsubst %.tex, %.ps, $(wildcard *.tex))
 PDF_FILES := $(patsubst %.tex, %.pdf, $(wildcard *.tex))
 ANSKEY_PDF := answer-key.pdf
+FILLER_PDF := filler.pdf
 
 .PHONY : plot dvi ps pdf install-pdfs clean
 
+filler : preview
+	@if [ -d ../fillers ] ; then \
+	gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -r700 -sOutputFile=fillerpage-%d.jpeg $(FILLER_PDF) ; \
+	for f in `ls fillerpage-*.jpeg`; do convert $$f -resize 600x800 -blur 5x2 $$f ; done \
+	fi
+
 preview : pdf
 	@if [ -d ../preview ] ; then \
-	gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -r700 -sOutputFile=page-%d.jpeg $(ANSKEY_PDF) ; \
-	for f in `ls page-*.jpeg`; do convert $$f -resize 600x800 $$f ; done \
+	gs -dNOPAUSE -dBATCH -sDEVICE=jpeg -r700 -sOutputFile=anskeypage-%d.jpeg $(ANSKEY_PDF) ; \
+	for f in `ls anskeypage-*.jpeg`; do convert $$f -resize 600x800 $$f ; done \
 	fi
 
 pdf : ps $(PDF_FILES)
