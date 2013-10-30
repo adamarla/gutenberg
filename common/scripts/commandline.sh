66b9222ac87584c2197e8b5d616c69307af9593e
Abhinav Chaturvedi <abhinav@gradians.com>
2013-10-30 18:12:52 +0530

    New command connect_with_common

--- common/scripts/commandline.sh
+++ common/scripts/commandline.sh
@@ -1,22 +1,31 @@
 
 #!/bin/bash
 
-function remake_links_in_vault { 
-  vault=$(find . -name "vault")
-  if [ -z "$vault" ] ; then 
-    echo "[Warning]: vault/ not in hierarchy"
+function connect_with_common { 
+  if [ -z "$1" ] ; then 
+    echo "[Error]: Specify a target ( vault | minthril )"
     return 0
   fi
 
-  parent=$(dirname $vault)
+  target=$(find . -name $1)
+  if [ -z "$target" ] ; then 
+    echo "[Warning]: $1/ not in hierarchy"
+    return 0
+  fi
+
+  parent=$(dirname $target)
   cd $parent
 
-  for f in `ls -d vault/*/*/*` ; do
+  for f in `ls -d $target/*/*/*` ; do
     if [ -e $f/Makefile ] ; then rm -f $f/Makefile ; fi
     if [ -e $f/shell-script ] ; then rm -f $f/shell-script ; fi
     
     cd $f
-      ln -s ../../../../$parent/common/makefiles/vault.mk Makefile 
+      if [ "$1" == "vault" ] ; then 
+        ln -s ../../../../$parent/common/makefiles/vault.mk Makefile 
+      else 
+        ln -s ../../../../$parent/common/makefiles/compile.mk Makefile 
+      fi
       ln -s ../../../../$parent/common/scripts/compile.sh shell-script 
     cd -
   done
