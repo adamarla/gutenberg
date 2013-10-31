0657f30ebadb053d7352bffa5a7db413be85c260
Abhinav Chaturvedi <abhinav@gradians.com>
2013-10-31 21:59:43 +0530

    Buttressed sanity checks + added colour

--- common/scripts/compile.sh
+++ common/scripts/compile.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
 function create_tex_from_blueprint {
   in_production=false
   if [ -e $1 ] ; then echo "[$1] -> Already present. Not re-creating" ; return 0 ; fi
@@ -7,6 +17,11 @@ function create_tex_from_blueprint {
 
   if [ -e /opt/gutenberg/PRODUCTION_SERVER ] ; then
     VAULT=/home/gutenberg/bank/vault
+  else
+    if [ -z $VAULT ] ; then 
+      echo -e "$COL_RED Environment variable VAULT not defined $COL_RESET. Define it, then continue"
+      return 0
+    fi
   fi
 
   insert_preamble $1
