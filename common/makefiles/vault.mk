b3678f5e2f2a646e9266430e3618e88cb1207673
Abhinav Chaturvedi <abhinav@gradians.com>
2014-10-03 01:11:28 +0530

    Get the exact answer key span using pdfinfo ...
    And send the span back in a curl request so that the Question
    object can be updated. Do this as part of question compilation.

--- common/makefiles/vault.mk
+++ common/makefiles/vault.mk
@@ -39,6 +39,7 @@ ifneq ($(logfile),)
 			echo "------ [$$version] -> Done" >> $(logfile)
 endif
 		done
+		curl $$(rails_server)/update/span?q=$$(relative_path)&n=$$(pdf_span)
 	else
 		compile_tex $< $(logfile)
 	fi
@@ -71,5 +72,5 @@ endif
 endif
 
 clean : 
-	rm -f $(STEM)* pg-* skel compiled
+	rm -f $(STEM)* pg-* skel compiled codex.cdx
 	rm -rf 0 1 2 3
