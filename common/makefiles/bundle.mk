7b707e8bbfdbb53a336e2e2246dbe95c79c7fd52
Akshay Damarla <akshay.damarla@gmail.com>
2015-11-04 13:34:53 +0530

    call to create question.zip

--- common/makefiles/bundle.mk
+++ common/makefiles/bundle.mk
@@ -8,6 +8,7 @@ bundle.xml : question.xml
 	if [ -e $@ ] ; then
 		if [ -e question.xml ] ; then
 			java -jar ~/quill/Qquill-all.jar -b
+			zip question.zip *.svg question.xml
 			touch $@
 		fi
 	fi
@@ -15,5 +16,5 @@ bundle.xml : question.xml
 question.xml :
 
 clean :
-	rm -f STMT_*.svg CTX_*.svg CRT_*.svg WRNG_*.svg RSN_*.svg CH_*.svg
+	rm -f STMT_*.svg CTX_*.svg CRT_*.svg WRNG_*.svg RSN_*.svg CH_*.svg PREVIEW.svg question.zip
 
