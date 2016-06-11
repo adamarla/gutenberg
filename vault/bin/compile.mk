40e5ea4084fa2660b08fe0da920b63d5e559bc30
Abhinav Chaturvedi <abhinav@gradians.com>
2016-06-11 11:51:49 +0530

    tex2svg command modified to take care of the whole flow

--- vault/bin/compile.mk
+++ vault/bin/compile.mk
@@ -1,13 +1,22 @@
 
 SHELL=/bin/bash
 .ONESHELL : 
-.PHONY : clean 
+.PHONY : clean tex2svg
 
 last_compiled_on : source.xml img_*.svg
 	@ quill -r $$(pwd) 
 	@ quill -p $$(pwd) 
 	@ ping_on_recompile -r 
 	@ date > $@
+
+tex2svg : source.pdf 
+	@ rm -f tex*.svg 
+	@ paper2svg $<
+
+source.pdf : source.tex
+	@ sed -i -e "s/\\previewon/\\previewoff/g" $<
+	@ latex --halt-on-error $< && dvips source.dvi && ps2pdf source.ps 
+	@ git add $<
 
 clean : 
 	@ ls | grep -e ^[[:digit:]] | xargs rm -f 
